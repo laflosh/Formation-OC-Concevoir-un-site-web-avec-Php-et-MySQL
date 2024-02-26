d9acf61c60f3514aae595257147936b7a3ee74cd
laflosh <l_florent_l@hotmail.fr>
2024-02-26 17:54:47 +0100

    Manipulation de tableau en php

--- index.php
+++ index.php
@@ -2,19 +2,27 @@
     $recettes = [
         [
             "titreRecette" => "Poulet Tikka Masala",
-            "auteurRecette" => "Emma Dupont"
+            "auteurRecette" => "Emma Dupont",
+            "recette" => "Etape 1: ...",
+            "disponible" => true
         ],
         [
             "titreRecette" => "Tarte Tatin aux Pommes",
-            "auteurRecette" => "Lucas Martin"
+            "auteurRecette" => "Lucas Martin",
+            "recette" => "Etape 1: ...",
+            "disponible" => true
         ],
         [
             "titreRecette" => "Spaghetti Carbonara",
-            "auteurRecette" => "Sophia Rodriguez"
+            "auteurRecette" => "Sophia Rodriguez",
+            "recette" => "Etape 1: ...",
+            "disponible" => false
         ],
         [
             "titreRecette" => "Chili Con Carne",
-            "auteurRecette" => "Antoine Leroy"
+            "auteurRecette" => "Antoine Leroy",
+            "recette" => "Etape 1: ...",
+            "disponible" => true
         ]
     ];
 ?>
@@ -29,35 +37,45 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Affichage des recettes</title>
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+        >
 
     </head>
     
     <body>
 
-        <h1>Recettes</h1>
-        
-        <p>
-            Retrouver toutes nos recettes de cuisines.<br />
-        </p>
+        <div class="container">
 
-        <ul>
+            <h1>Recettes</h1>
+            
+            <p>
+                Retrouver toutes nos recettes de cuisines.<br />
+            </p>
+
 
             <?php 
-                for($counter = 0; $counter < count($recettes); $counter++):
+
+                for($counter = 0; $counter < count($recettes); $counter++){
+
+                    $disponible = $recettes[$counter]["disponible"];
+
+                    if($disponible){
+                        echo ("            
+                        <article>
+
+                            <h2>{$recettes[$counter]["titreRecette"]}</h2>
+            
+                            <div>{$recettes[$counter]["recette"]}</div>
+                            <i>{$recettes[$counter]["auteurRecette"]}</i>
+        
+                        </article>");
+                    };
+                };
             ?>
 
-            <li>
-
-                <h2><?php echo $recettes[$counter]["titreRecette"]?></h2>
-                <p><?php echo $recettes[$counter]["auteurRecette"]?></p>
-
-            </li>
-
-            <?php
-                endfor; 
-            ?>
-
-        </ul>
+        </div>
 
     </body>
 
